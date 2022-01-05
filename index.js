67f3f2ec2955296b0b2e1289f32254f0cebab50a
Alix Virot <flying.cat@mac.com>
2022-01-05 19:51:59 +0100

    Miss the last one

--- index.js
+++ index.js
@@ -1,23 +1,81 @@
+// npm i 
+// node index.js 
+// nodemon index.js
+
+// Don't forget :
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+// My own database adress, mon compass à moi.
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
+mongoose.connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
+    // => Connected to the database: "recipe-app" ??
+    // Before adding any recipes to the database, let's remove all existing ones :
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.create({ // CREATE cf const Recipe = mongoose.model('Recipe', recipeSchema);
+      title: "Charlotte aux fraises ",
+      level: "Amateur Chef",
+      ingredients: [
+        "1/2 cup sucre",
+        "5 tablespoons honey",
+        "1/3 cup vanille",
+        "1/4 butter",
+        "3 milk",
+        "3 farine",
+        "fraises",
+      ],
+      cuisine: "French",
+      dishType: "dessert",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Alix",
+    })
+  .then((res)=> console.log(res.title)) // => Charlotte aux fraises OK
+  .catch((error)=> console.log("error",error))
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+
+  .then(() => {
+    Recipe.insertMany(data) // ADD many docs 
+    .then( (recipeDoc) => {
+      recipeDoc.forEach((recipe)=> {
+        console.log(recipe.title); // => 6 recipes logués OK
+      }); // No catch ??
+      Recipe.findOneAndUpdate( // FIND rigatoni & UPDATE duration
+        { title: 'Rigatoni alla Genovese' }, 
+        { duration: 100 }, 
+        {new:true}
+        ).then((res)=> console.log(res)) // modif duration OK
+        .catch((error)=>console.error("error",error));
+
+        Recipe.deleteOne( // DELETE ONE
+          {title: "Carrot Cake"}) 
+      .then((res)=> console.log(res)) // Ai-je réussi ? NON, mais "deletedCount: 1" ?
+      .catch((error)=>console.error("error",error));
+      })
+      
+      .catch((error)=>console.error("error",error)); // que fais-tu là ? InsertMany ?
+    })
+
+    .catch(error => {
+      console.error('Error connecting to the database', error);
+      })
+  
+    
+  
+ 
+
+
+
+ 
+
